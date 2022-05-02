cc91b992462838d65a4a0c033df783ec57bb56a1
edao3 <102071708+edao3@users.noreply.github.com>
2022-05-02 17:09:40 -0500

    Update loadCUDA.py

--- loadCUDA.py
+++ loadCUDA.py
@@ -1,2 +1,41 @@
 import numpy as np
 import torch
+
+imsize = 512 if torch.cuda.is_available() else 128	#128 is a smaller size if there is no GPU
+
+loader = transforms.Compose([
+    transforms.Resize(imsize),	#this scales the imported image
+    transforms.ToTensor()])		#transformation to a torch sensor
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)	#this is a fake batch dimension
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+style_img = image_loader('./PICTURE')	#insert image name here
+content_img = image_loader('./PICTURE')
+
+assert style_img.size() == content_img.size(), \
+    "we need to import the art and original images of identical size"
+
+unloader = transforms.ToPILImage()
+
+plt.ion()
+
+def imshow(tensor, title = Picture):
+    image = tensor.cpu().clone()	#we clone this to avoid changes to it
+    image = image.squeeze(0)		#removes to fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not Picture:
+        plt.title(title)
+    plt.pause(0.005)	#delay to update plots
+
+
+plt.figure()
+imshow(style_img, title='Art Image')
+
+plt.figure()
+imshow(content_img, title='Original Image')
