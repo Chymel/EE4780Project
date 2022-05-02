564edbb1dfd695c4cf3ace9c39466759178eacc1
edao3 <102071708+edao3@users.noreply.github.com>
2022-05-02 18:28:52 -0500

    Update loadCUDA.py

--- loadCUDA.py
+++ loadCUDA.py
@@ -2,9 +2,10 @@ import numpy as np
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
+from matplotlib import pyplot as plt
 
-content_img = Image.open('iceland.jpg')
-style_img = Image.open('starrynight.jpg')
+content_img = Image.open('C:/Users/judyd/Documents/LSU/Spring 2022/tanjiro.jpg')
+style_img = Image.open('C:/Users/judyd/Documents/LSU/Spring 2022/starrynight.jpg')
 
 # content image information
 plt.imshow(content_img)
@@ -26,6 +27,7 @@ img_tensortwo =transform(style_img)
 print(img_tensor)
 print(img_tensortwo)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 imsize = 512 if torch.cuda.is_available() else 128	#128 is a smaller size if there is no GPU
 
@@ -40,15 +42,18 @@ def image_loader(image_name):
     return image.to(device, torch.float)
 
 
-style_img = image_loader('./PICTURE')	#insert image name here
-content_img = image_loader('./PICTURE')
+style_img = image_loader('C:/Users/judyd/Documents/LSU/Spring 2022/starrynight.jpg')	#insert image name here
+content_img = image_loader('C:/Users/judyd/Documents/LSU/Spring 2022/tanjiro.jpg')
 
-assert style_img.size() == content_img.size(), \
-    "we need to import the art and original images of identical size"
 
 unloader = transforms.ToPILImage()
 
 plt.ion()
+
+
+class Picture:
+    pass
+
 
 def imshow(tensor, title = Picture):
     image = tensor.cpu().clone()	#we clone this to avoid changes to it
